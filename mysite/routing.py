5f760b923c96ad313fb25e70748507eef41bdee4
adityakaushik99 <30402498+adityakaushik99@users.noreply.github.com>
2020-10-25 19:02:26 +0530

    Add routing.py

---
+++ mysite/routing.py
@@ -0,0 +1,12 @@
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from firstPage import consumer
+
+websocket_urlPattern = [
+    path('ws/polData/',consumer.Consumer),
+]
+
+application=ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(URLRouter(websocket_urlPattern))
+})
